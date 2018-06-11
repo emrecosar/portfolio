019f3e3ac7465080bf1c0353d72b3e6590d319ef
Emre Cosar <emrecosarr@gmail.com>
2018-06-11 10:40:45 +0300

    change css and wording

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -32,7 +32,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. <br /> {description}.</h3>
+            <h3>I'm {city} based <span>{occupation}</span>. <br /> {description}.</h3>
             <hr />
             <ul className="social">
                {networks}
