2e0abcf295066df3a5a65228c3b84d6a38043510
Emre Cosar <emrecosarr@gmail.com>
2018-07-08 23:36:49 +0300

    Added og tags and fix typo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,6 +7,7 @@ class Footer extends Component {
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
+      var name = this.props.data.name;
     }
 
     return (
@@ -19,7 +20,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2018 Emre Cosar</li>
+              <li>&copy; Copyright 2018 {name}</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -31,8 +31,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm {city} based <span>{occupation}</span>. <br /> {description}.</h3>
+            <h1 className="responsive-headline">{name}.</h1>
+            <h3>{city} based <span>{occupation}</span>. <br /> {description}.</h3>
             <hr />
             <ul className="social">
                {networks}
