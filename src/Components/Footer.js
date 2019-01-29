f9fa9f0118556e12667c7cee718e823ab33c6cff
Emre Cosar <emrecosarr@gmail.com>
2019-01-29 20:49:32 +0100

    updated footer js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2018 {name}</li>
+              <li>&copy; Copyright 2019 {name}</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
