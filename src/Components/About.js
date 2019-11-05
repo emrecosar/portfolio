bddd6dd18f580fa7e08bd2e1205da6848d155bc9
Emre Cosar <emrecosarr@gmail.com>
2019-11-05 22:26:40 +0100

    npm update, bio update, robots.txt added

--- src/Components/About.js
+++ src/Components/About.js
@@ -35,7 +35,7 @@ class About extends Component {
               </div>
               <div className="columns download">
                 <p>
-                  <a href={resumeDownload} className="button" target="_blank"><i className="fa fa-download"></i>Download Resume</a>
+                  <a href={resumeDownload} className="button" target="_blank" rel="noopener noreferrer"><i className="fa fa-download"></i>Download Resume</a>
                 </p>
                </div>
             </div>
