905c69fe432c0d2a79736f43a5aec192c41e9100
Emre Cosar <emrecosarr@gmail.com>
2019-11-05 22:27:32 +0100

    Updates

---
+++ precache-manifest.b254db9f93ecac8283696e4bdf7a04c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec510ea4f041d2773a78cd35582f23c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bf2fb75e3941f8e2392",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "af9a900b1ea4aae8ec3e",
+    "url": "/static/js/2.368eef8c.chunk.js"
+  },
+  {
+    "revision": "9bf2fb75e3941f8e2392",
+    "url": "/static/js/main.e7d4ea5c.chunk.js"
+  },
+  {
+    "revision": "05129d9571d833ef1980",
+    "url": "/static/js/runtime-main.a5eaf332.js"
+  }
+]);
