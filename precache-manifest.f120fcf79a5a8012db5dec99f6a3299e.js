6276106c999057af7f6b0cb082a9cdc31dcc9a1c
emrecosar <emrecosar@users.noreply.github.com>
2022-03-07 21:49:50 +0000

    Deploying to gh-pages from @ emrecosar/portfolio@0c673161a4fe866c8a80436240be0185b4acfb38 🚀

--- precache-manifest.f120fcf79a5a8012db5dec99f6a3299e.js
+++ precache-manifest.f120fcf79a5a8012db5dec99f6a3299e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c8b72bad36b14d45cab14b1e213c9369",
+    "revision": "64de24a968b2aff4349cccdeb2f357bd",
     "url": "/index.html"
   },
   {
-    "revision": "3a0794fa7bcb041a0a43",
+    "revision": "5b3c3efe1244c7f3b487",
     "url": "/static/css/main.5facb584.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.0a9fb44b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3a0794fa7bcb041a0a43",
-    "url": "/static/js/main.5a3588c0.chunk.js"
+    "revision": "5b3c3efe1244c7f3b487",
+    "url": "/static/js/main.b2db94b8.chunk.js"
   },
   {
     "revision": "4f2b1704eab49d50853b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bff41bcf07ba6ed5e501e8958fabb75.js"
+  "/precache-manifest.f120fcf79a5a8012db5dec99f6a3299e.js"
 );
 
 self.addEventListener('message', (event) => {
