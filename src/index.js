29b97bf7441cc32d45e55fb880c62131bfeb7d2a
Emre Cosar <emrecosar@emres-mbp.lan>
2026-01-10 18:02:41 +0100

    new background with webp and package json updates with gemini pro

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-0N5C0TE0HR');
+ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
